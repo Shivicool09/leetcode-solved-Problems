8dce98b722ec1b3f4ac2940e674a0edd6058c208
SHIVIKA BANSAL <93571647+Shivika0911@users.noreply.github.com>
2023-07-02 14:53:22 +0530

    Time: 12 ms (56.29%), Space: 56.2 MB (31.00%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,45 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = findMid(head);
+        ListNode rightHead = mid.next;
+        mid.next = null;
+        ListNode sortedLeft = sortList(head);
+        ListNode sortedRight = sortList(rightHead);
+        return merge(sortedLeft, sortedRight);
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode mergedLL = new ListNode(-1);
+        ListNode temp = mergedLL;
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+        if (head1 != null) {
+            temp.next = head1;
+        }
+        if (head2 != null) {
+            temp.next = head2;
+        }
+        return mergedLL.next;
+    }
+}
