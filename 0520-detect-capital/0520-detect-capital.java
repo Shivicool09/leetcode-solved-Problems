1f9df62fc41f4aafde19f25618e30cd1b0d96b80
SHIVIKA BANSAL <93571647+Shivika0911@users.noreply.github.com>
2023-07-15 10:26:45 +0530

    Time: 1 ms (100.00%), Space: 41 MB (55.51%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,48 +1,49 @@
-class Solution {
+public class Solution {
     public boolean detectCapitalUse(String word) {
-        int n=word.length();
-        boolean case1=true,case2=true,case3=true;
+        int n = word.length();
         
-        
-        for(int i=0;i<n;i++){
-            if(!Character.isUpperCase(word.charAt(i))){
-                 case1= false;
+        // Check if all characters are uppercase
+        boolean allUpperCase = true;
+        for (int i = 0; i < n; i++) {
+            if (!isUpperCase(word.charAt(i))) {
+                allUpperCase = false;
                 break;
             }
         }
-            if(case1){
-                return true;
-            }
-        
-        for(int i=0;i<n;i++){
-        
-        
-         if(!Character.isLowerCase(word.charAt(i))){
-            case2= false;
-             break;
-        }
-        }
-        
-            if(case2){
-        return true;
-            }
-        
-        
-            if(!Character.isUpperCase(word.charAt(0))){
-                case3=false;
-            }
-            if(case3){
-                for(int i=1;i<n;i++){
-                    if(!Character.isLowerCase(word.charAt(i))){
-                        case3=false;
-                        break;
-                    }
-                }
-      
-            }
-        if(case3){
+        if (allUpperCase) {
             return true;
         }
+        
+        // Check if all characters are lowercase
+        boolean allLowerCase = true;
+        for (int i = 0; i < n; i++) {
+            if (!isLowerCase(word.charAt(i))) {
+                allLowerCase = false;
+                break;
+            }
+        }
+        if (allLowerCase) {
+            return true;
+        }
+        
+        // Check if only the first character is uppercase
+        if (isUpperCase(word.charAt(0))) {
+            for (int i = 1; i < n; i++) {
+                if (!isLowerCase(word.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
         return false;
+    }
+    
+    public boolean isUpperCase(char ch) {
+        return ch >= 'A' && ch <= 'Z';
+    }
+    
+    public boolean isLowerCase(char ch) {
+        return ch >= 'a' && ch <= 'z';
     }
 }
