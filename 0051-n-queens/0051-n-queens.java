859c164f4fd28337e0821c08a6c863a67ef95c9d
SHIVIKA BANSAL <93571647+Shivika0911@users.noreply.github.com>
2023-07-29 15:01:27 +0530

    Time: 2 ms (90.95%), Space: 44.2 MB (46.68%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char board[][] = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        List<List<String>> ans = new ArrayList<>();
+        nQueens(ans, board, 0);
+        return ans;
+    }
+
+    public static void nQueens(List<List<String>> ans, char board[][], int row) {
+        if (row == board.length) {
+            printBoard(ans, board);
+            return;
+        }
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(ans, board, row + 1);
+                board[row][j] = '.';
+            }
+        }
+    }
+
+    public static boolean isSafe(char board[][], int row, int col) {
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printBoard(List<List<String>> ans, char board[][]) {
+        List<String> solution = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < board.length; j++) {
+                sb.append(board[i][j]);
+            }
+            solution.add(sb.toString());
+        }
+        ans.add(solution);
+    }
+}
