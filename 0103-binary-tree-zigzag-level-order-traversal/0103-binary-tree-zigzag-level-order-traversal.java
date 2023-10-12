f7696903c297d2f2eaad4327cbc7d934aeac35ca
SHIVIKA BANSAL <93571647+Shivika0911@users.noreply.github.com>
2023-10-12 23:42:48 +0530

    Time: 1 ms (90.48%), Space: 40.8 MB (91.56%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        boolean leftToRight = true; // Flag to track the order of traversal
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> levelValues = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                if (leftToRight) {
+                    TreeNode node = queue.poll();
+                    levelValues.add(node.val);
+
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                } else {
+                    TreeNode node = queue.pollLast();
+                    levelValues.add(node.val);
+
+                    if (node.right != null) {
+                        queue.offerFirst(node.right);
+                    }
+                    if (node.left != null) {
+                        queue.offerFirst(node.left);
+                    }
+                }
+            }
+
+            result.add(levelValues);
+            leftToRight = !leftToRight; // Toggle the direction for the next level
+        }
+
+        return result;
+    }
+   
+    }
