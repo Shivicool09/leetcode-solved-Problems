85e32eec80a9c90439c9ffe7a9514fc4d2699185
SHIVIKA BANSAL <93571647+Shivika0911@users.noreply.github.com>
2023-11-04 14:05:14 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (30.55%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String[] rows = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+        List<String> res = new ArrayList();
+        boolean flag = true;
+
+        for(String word : words){
+            for(String row : rows){
+                for(int i=0; i <word.length(); i++){
+                    if(row.indexOf(word.toLowerCase().charAt(i)) == -1) {
+                        flag = false;
+                        break;
+                    }
+                    flag=true;
+                }
+                if(flag){
+                    res.add(word);
+                }
+            }
+        }
+        return res.toArray(new String[0]);
+        
+    }
+}
